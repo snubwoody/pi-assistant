2f848eaa7f508581ca1aa50d4aae86917bdc4e61
waku <wakunguma13@gmail.com>
2023-04-08 11:13:40 +0200

    removed api key from files

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ from audio import speak
 
 
 def gpt(chat_prompt:str):
-    api_key:str = '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'
 
     openai.api_key = api_key
     response = openai.Completion.create(model="text-davinci-003", prompt=chat_prompt, temperature=0.2, max_tokens=50)
--- src/test.py
+++ src/test.py
@@ -1,17 +1,3 @@
-import time
-
-#TODO make this shit work man
-def timer(func):
-    def wrapper(*args):
-        start = time.perf_counter()
-        func()
-        end = time.perf_counter()
-        print(f'{func.__name__} took {end - start:0.1f}s')
-        
-    return wrapper
-
-@timer
-def p(k):
+with open('.txt') as f:
+    k = f.read()
     print(k)
-
-p(9)
